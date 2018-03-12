32f1795c28ab9ba571eac080598e8d18c59467fe
Nick Kasprzak <nkasprzak@iland.com>
2018-03-11 22:48:49 -0500

    updated soup.py

--- soup.py
+++ soup.py
@@ -1,9 +1,19 @@
 from bs4 import BeautifulSoup
+import os,glob
 
-soup = BeautifulSoup(open("/home/nkasprzak/houston-music/songkick/songkick-108-warehouse-live.html"), "html.parser")
+files = []
 
-for li in soup.find_all('li'):
-	if li.get('title') != None:
-		print li.find('time').get('datetime')
-		print li.find('p').find('strong').get_text()
-		print '\n'
+os.chdir("/home/nkasprzak/houston-music/songkick")
+for file in glob.glob("*.html"):
+    files.append(file)
+
+for venue in files:
+
+	soup = BeautifulSoup(open("/home/nkasprzak/houston-music/songkick/%s" % venue), "html.parser")
+
+	for li in soup.find_all('li'):
+		if li.get('title') != None:
+			print li.find('time').get('datetime')
+			print li.find('p').find('strong').get_text()
+	
+	print '\n'
